6784cfcab3aa6a98be4173837a3974ac8b3d958e
hrawp <epiihrawp@gmail.com>
2024-08-30 17:53:06 -0500

    Iteration 2 - custom

--- utils_hrawp.py
+++ utils_hrawp.py
@@ -1,27 +1,17 @@
-''' ITERATION 1
+''' ITERATION 2
 
 Data Analysis for Mars Landing
 
 This module provides a simple, reusable foundation for my analytics projects. 
-When we work hard to write useful code, we want it to be reusable.
-A good byline could be used in every Python analytics project we do.
+
 
 Process:
 
-We don't write code from top to bottom; instead, we often write it from the outside in.
-Here's what a first draft of my utils_hra.py might look like:
+In this second interation, add a function that returns the byline as a string.
+I'll create a function named get_byline().
+It'll return my byline to whatever calls the get_byline() function.
+I'll update the main() function to use the new get_byline() function.
 
-1. I start with this docstring at the very beginning.
-   I use it to clarify the purpose of my Python file and organize my thoughts.
-   
-2. I'll declare a global variable for my byline string and just set it to some simple text.
-
-3. I'll declare a main() function for my module. When I run this script, I can use main() to test my byline.
-
-4. I'll add the boilerplate conditional execution code so I only run the main() function when 
-   this script is executed directly (but not when I import it into another file).
-
-I'll test it in an online interpreter to ensure this version runs correctly before continuing.
 '''
 
 #####################################
@@ -30,6 +20,14 @@ I'll test it in an online interpreter to ensure this version runs correctly befo
 
 byline: str = 'Using Data to Land on Mars'
 
+#####################################
+# Define the get_bylein() Function
+#####################################
+
+def get_byline() -> str:
+   '''Return a byline for my anlystics projects.'''
+   return byline
+   
 #####################################
 # Define a main() function for this module.
 #####################################
@@ -44,8 +42,8 @@ byline: str = 'Using Data to Land on Mars'
 # Everything afer the colon (:) must be indented (usually 4 spaces)
 
 def main() -> None:
-    '''Print the byline to the console when this function is called.'''
-    print(byline)
+     '''Print the byline to the console when this function is called.'''
+     print(get_byline())
 
 #####################################
 # Conditional Execution - Only call main() when executing this module as a script.
